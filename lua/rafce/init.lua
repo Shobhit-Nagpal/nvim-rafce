d20d8aa90ca038ba74975228c2e167ed7eadc681
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-04-02 21:28:44 +0530

    Added more commands for different varations

--- lua/rafce/init.lua
+++ lua/rafce/init.lua
@@ -5,7 +5,37 @@ vim.api.nvim_create_user_command(
   function()
     local filename = vim.api.nvim_buf_get_name(0)
     componentName = utils.getComponentName(filename)
-    utils.writeComponent(componentName)
+    utils.writeRafceComponent(componentName)
   end,
-  {bang = true, desc = "A command to create boilerplate code of a React functional component."}
+  {bang = true, desc = "Command to create boilerplate code of a React functional component with arrow function and export at bottom."}
+)
+
+vim.api.nvim_create_user_command(
+  "Rafc", 
+  function()
+    local filename = vim.api.nvim_buf_get_name(0)
+    componentName = utils.getComponentName(filename)
+    utils.writeRafcComponent(componentName)
+  end,
+  {bang = true, desc = "Command to create boilerplate code of a React functional component with arrow function."}
+)
+
+vim.api.nvim_create_user_command(
+  "Rfce", 
+  function()
+    local filename = vim.api.nvim_buf_get_name(0)
+    componentName = utils.getComponentName(filename)
+    utils.writeRfceComponent(componentName)
+  end,
+  {bang = true, desc = "Command to create boilerplate code of a React functional component with export at bottom."}
+)
+
+vim.api.nvim_create_user_command(
+  "Rfc", 
+  function()
+    local filename = vim.api.nvim_buf_get_name(0)
+    componentName = utils.getComponentName(filename)
+    utils.writeRfcComponent(componentName)
+  end,
+  {bang = true, desc = "Command to create boilerplate code of a React functional component."}
 )
--- lua/rafce/utils.lua
+++ lua/rafce/utils.lua
@@ -1,3 +1,5 @@
+local extensions = {"js", "ts", "jsx", "tsx"};
+
 local function split (inputstr, sep)
   if sep == nil then
     sep = "%s"
@@ -26,19 +28,33 @@ local function getComponentName(filename)
 
   fileParts = split(filename, "/")
   currentFilename = split(fileParts[#fileParts], ".")
+  fileExtension = currentFilename[#currentFilename]
+
+  local found = false
+
+  for _, exten in ipairs(extensions) do
+    if fileExtension == exten then
+      found = true
+    end
+  end
+
+  if not found then
+    error("Error: Current file is not a jsx or tsx file.")
+  end
+
   componentName = currentFilename[1]
   return componentName
 
 end
 
-local function writeComponent(componentName)
+local function writeRafceComponent(componentName)
 
   lines = {
   "import React from 'react';",
   "",
   "const " ..componentName.. " = () => {", 
   "  return <div>" ..componentName.. "</div>;",
-  "}",
+  "};",
   "",
   "export default " ..componentName .. ";",
   }
@@ -47,7 +63,53 @@ local function writeComponent(componentName)
 
 end
 
+local function writeRafcComponent(componentName)
+
+  lines = {
+  "import React from 'react';",
+  "",
+  "export default const " ..componentName.. " = () => {", 
+  "  return <div>" ..componentName.. "</div>;",
+  "};",
+  }
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+
+end
+
+local function writeRfceComponent(componentName)
+
+  lines = {
+  "import React from 'react';",
+  "",
+  "function " ..componentName.. "() {", 
+  "  return <div>" ..componentName.. "</div>;",
+  "};",
+  "",
+  "export default " ..componentName .. ";",
+  }
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+
+end
+
+local function writeRfcComponent(componentName)
+
+  lines = {
+  "import React from 'react';",
+  "",
+  "export default function " ..componentName.. "() {", 
+  "  return <div>" ..componentName.. "</div>;",
+  "};",
+  }
+
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+
+end
 return {
   getComponentName = getComponentName,
-  writeComponent = writeComponent
+  writeRafceComponent = writeRafceComponent,
+  writeRafcComponent = writeRafcComponent,
+  writeRfceComponent = writeRfceComponent,
+  writeRfcComponent = writeRfcComponent,
 }
